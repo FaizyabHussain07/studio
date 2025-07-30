41586b7e0268f8326475706a5feaf911a2e7a6ec
Faizyab Muhammad <muhammadfaizyab16@gmail.com>
2025-07-30 04:07:01 +0000

    .js:787             throw new Error(`Configuring Next.js via '${(0, _pat

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
