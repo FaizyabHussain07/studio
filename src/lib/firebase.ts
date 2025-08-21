3e7d425a36047682a199471ecabc70f4e2a58919
Faizyab Muhammad <muhammadfaizyab16@gmail.com>
2025-08-21 13:36:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,26 +1,20 @@
 
-// Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBBzXlMJjjFcqCCOY_C4Fs5DU3RDAKhKOA",
-  authDomain: "faizyab-al-quran.firebaseapp.com",
-  projectId:"faizyab-al-quran",
-  storageBucket: "faizyab-al-quran.appspot.com",
-  messagingSenderId: "772007792970",
-  appId: "1:772007792970:web:8cc29051b464d8684471b3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
-let app;
-if (!getApps().length) {
-    app = initializeApp(firebaseConfig);
-} else {
-    app = getApps()[0];
-}
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const auth = getAuth(app);
 
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+export { app, db, auth };
